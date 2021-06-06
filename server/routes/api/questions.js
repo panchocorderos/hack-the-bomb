17a42a14a10e9596560da2894769cc771175d769
panchocorderos <fco.cordero98@gmail.com>
2021-06-06 16:32:46 -0400

    some changes to api

--- server/routes/api/questions.js
+++ server/routes/api/questions.js
@@ -3,6 +3,21 @@ const router = express.Router();
 
 const { Question, Answer } =  require('../../db');
 
+
+const getCorrectAnswer = (answers) => {
+  for(let i = 0; i < answers.length; i++) {
+    if (answers[i].is_correct === 1) return i;
+  }
+}
+
+const answerArray = (answers) => {
+  let arrayAnswers = [];
+  for(let i = 0; i < answers.length; i++) {
+    arrayAnswers.push(answers[i].answer_text);
+  }
+  return arrayAnswers;
+}
+
 const questionIdArray = (questions) => {
   let arrayQuestions = [];
   for(let i = 0; i < questions.length; i++){
@@ -23,7 +38,6 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:levelId', async (req, res) => {
-
   try {
     const questions = await Question.findAll({ 
       where: {
@@ -32,10 +46,30 @@ router.get('/:levelId', async (req, res) => {
       attributes: ['id', 'question_text', 'levelId']
     });
     questionArr = questionIdArray(questions);
-    res.json({
-      "questions": questionArr,
-      "quantity": questionArr.length,
-    });
+    const answers = []
+    for (let i = 0; i < questionArr.length; i++) {
+      answers.push(await Answer.findAll({ 
+          where: { questionId: questionArr[i]},
+          raw: true,
+          attributes: ['id', 'answer_text', 'is_correct', 'questionId']
+        })
+      );
+    }
+    // console.log(answers)
+
+    const response = [];
+
+    for (let i = 0; i < questions.length; i++) {
+      response.push({
+        "question": questions[i].question_text,
+        "answers": answerArray(answers[i]),
+        "correct": getCorrectAnswer(answers[i]),
+      })
+    }
+    // console.log(response)
+
+    
+    res.json(response);
   }
   catch (err) {
     res.status(404);
